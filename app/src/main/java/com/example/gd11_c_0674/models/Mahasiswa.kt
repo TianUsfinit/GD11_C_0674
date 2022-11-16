549aabd53f0022d7baecf576b5116e145a4d9a90
TianUsfinit <112057192+TianUsfinit@users.noreply.github.com>
2022-11-16 15:13:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gd11_c_0674/models/Mahasiswa.kt
@@ -0,0 +1,10 @@
+package com.example.gd11_c_0674.models
+
+class Mahasiswa(
+    var nama: String,
+    var npm: String,
+    var fakultas: String,
+    var prodi: String
+) {
+    var id: Long? = null
+}
